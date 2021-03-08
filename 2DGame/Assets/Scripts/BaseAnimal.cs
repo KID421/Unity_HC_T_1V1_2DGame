410a817525a3ae4823e93899790f879fc54dd4b2
KID <50666207+KID421@users.noreply.github.com>
2021-03-08 16:53:19 +0800

    生成系統與動物基底

---
+++ 2DGame/Assets/Scripts/BaseAnimal.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class BaseAnimal : MonoBehaviour
+{
+    [Header("行動速度")]
+    public float speed;
+    [Header("生命")]
+    public float lifetime;
+    [Header("吃幾個目標物會生成下一隻")]
+    public int eatCount;
+    [Header("要追蹤的目標名稱")]
+    public string targetName;
+    [Header("目標物件陣列")]
+    public GameObject[] targets;
+
+    /// <summary>
+    /// 搜尋目標
+    /// </summary>
+    private void FindTarget()
+    {
+        // 目標物件陣列 = 遊戲物件.透過標籤尋找物件(目標名稱)
+        targets = GameObject.FindGameObjectsWithTag(targetName);
+    }
+
+    private void Start()
+    {
+        FindTarget();
+    }
+}
--- 2DGame/Assets/Scripts/SpawnManager.cs
+++ 2DGame/Assets/Scripts/SpawnManager.cs
@@ -10,19 +10,36 @@ public class SpawnManager : MonoBehaviour
     // 欄位語法
     // 修飾詞 類型 名稱 指定 值；
     // public 公開 顯示在面板
+    [Header("間隔")]
     public float interval = 2.5f;
 
-    // 存放物件可以用 GameObject
-    public GameObject apple;
+    [Header("生成物件")]
+    public GameObject obj;
+
+    [Header("設定左右與上下生成範圍")]
+    public Vector2 posMin = new Vector2(-8, -4);
+    public Vector2 posMax = new Vector2(+8, +4);
+
+    [Header("第一次生成的數量")]
+    public int count;
+
+    [Header("是否需要間隔生成")]
+    public bool needSpawn;
+    [Header("目前物件的數量")]
+    public int currentCount;
 
     // 事件 - 入口
     // 開始：播放時執行一次
     private void Start()
     {
-        // 輸出 API 測試
-        print("測試");
+        // 第一次生成
+        for (int i = 0; i < count; i++)
+        {
+            Spawn();
+        }
 
-        Spawn();
+        // 如果 需要間隔生成 重複呼叫("函式名稱"，開始時間，間隔時間)
+        if (needSpawn) InvokeRepeating("Spawn", interval, interval);
     }
 
     // 函式 - 包含程式區塊
@@ -30,7 +47,15 @@ public class SpawnManager : MonoBehaviour
     // 修飾詞 類型 名稱 () { 程式區塊 }
     public void Spawn()
     {
-        // 生成(物件)
-        Instantiate(apple);
+        // 數量遞增
+        currentCount++;
+
+        // 取得隨機座標 x 與 y
+        float x = Random.Range(posMin.x, posMax.x);
+        float y = Random.Range(posMin.y, posMax.y);
+
+        // 生成(物件，座標，角度)
+        // 零角度 Quaternion.identity
+        Instantiate(obj, new Vector2(x, y), Quaternion.identity);
     }
 }
