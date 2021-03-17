e82c7f387ba2681729ad6f1b8ff77940aef66814
KID <50666207+KID421@users.noreply.github.com>
2021-03-17 15:29:03 +0800

    添加介面與數據儲存

--- 2DGame/Assets/Scripts/GameManager.cs
+++ 2DGame/Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
-﻿using UnityEngine;
-using System;
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -27,9 +28,99 @@ public class GameManager : MonoBehaviour
     public float aboiginalBadEyeLife;
     public int aboiginalBadEyeEat;
 
+    [Header("青蛙 擁有保護色")]
+    public Frog objFrogHaveColor;
+    [Header("青蛙 沒有保護色")]
+    public Frog objFrogNoColor;
+    [Header("原住民 視力良好")]
+    public Aboriginal objAboriginalGoodEye;
+    [Header("原住民 視力不好")]
+    public Aboriginal objAboriginalBadEye;
+
     public void AppleFirst(string getValue)
     {
         // 整數 = 整數.轉型(字串) - 將字串轉為整數
         appleFirst = Int32.Parse(getValue);
+    }
+
+    public void AppleInterval(string getValue)
+    {
+        appleInterval = Int32.Parse(getValue);
+    }
+
+    public void FrogNoColor(string getValue)
+    {
+        frogNoColor = Int32.Parse(getValue);
+    }
+
+    public void FrogNoColorLife(string getValue)
+    {
+        frogNoColorLife = Int32.Parse(getValue);
+        objFrogNoColor.lifetime = frogNoColorLife;                  // 更新 沒有保護色 生命
+    }
+
+    public void FrogNoColorEat(string getValue)
+    {
+        frogNoColorEat = Int32.Parse(getValue);
+        objFrogNoColor.eatCount = frogNoColorEat;                   // 更新 沒有保護色 吃幾個
+    }
+
+    public void FrogHaveColor(string getValue)
+    {
+        frogHaveColor = Int32.Parse(getValue);
+    }
+
+    public void FrogHaveColorLife(string getValue)
+    {
+        frogHaveColorLife = Int32.Parse(getValue);
+        objFrogHaveColor.lifetime = frogHaveColorLife;              // 更新 擁有保護色 生命
+    }
+
+    public void FrogHaveColorEat(string getValue)
+    {
+        frogHaveColorEat = Int32.Parse(getValue);
+        objFrogHaveColor.eatCount = frogHaveColorEat;               // 更新 擁有保護色 吃幾個
+    }
+
+    public void AboiginalGoodEye(string getValue)
+    {
+        aboiginalGoodEye = Int32.Parse(getValue);
+    }
+
+    public void AboiginalGoodEyeLife(string getValue)
+    {
+        aboiginalGoodEyeLife = Int32.Parse(getValue);
+        objAboriginalGoodEye.lifetime = aboiginalGoodEyeLife;       // 更新 視力良好 生命
+    }
+
+    public void AboiginalGoodEyeEat(string getValue)
+    {
+        aboiginalGoodEyeEat = Int32.Parse(getValue);
+        objAboriginalGoodEye.eatCount = aboiginalGoodEyeEat;        // 更新 視力良好 吃幾個
+    }
+
+    public void AboiginalBadEye(string getValue)
+    {
+        aboiginalBadEye = Int32.Parse(getValue);
+    }
+
+    public void AboiginalBadEyeLife(string getValue)
+    {
+        aboiginalBadEyeLife = Int32.Parse(getValue);
+        objAboriginalBadEye.lifetime = aboiginalBadEyeLife;         // 更新 視力不好 生命
+    }
+
+    public void AboiginalBadEyeEat(string getValue)
+    {
+        aboiginalBadEyeEat = Int32.Parse(getValue);
+        objAboriginalBadEye.eatCount = aboiginalBadEyeEat;          // 更新 視力不好 吃幾個
+    }
+
+    /// <summary>
+    /// 載入場景
+    /// </summary>
+    public void LoadScene()
+    {
+        SceneManager.LoadScene("遊戲場景");
     }
 }
